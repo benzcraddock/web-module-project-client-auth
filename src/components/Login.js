23f94ae94e0891800fe252cff1cc6d3b754f9d7f
Benjamin Craddock <benjamincraddock@benjamins-mbp.lan>
2021-12-14 16:42:19 -0500

    add axios post to api and redirect from login to friend

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+// for redirect
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
 
 const Login = () => {
+
+  // for redirect
+  const { push } = useHistory();
+
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: ""
+  });
+
+  const handleChange = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]:e.target.value
+    });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('http://localhost:9000/api/login', credentials)
+      .then(res => {
+        console.log(res);
+        localStorage.setItem("token", res.token);
+        push('/friend');
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }
+
   return (
     <div>
       <h1>LOGIN</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="username">USERNAME</label>
-          <input id="username"/>
+          <input onChange={handleChange} name="username" id="username"/>
         </div>
 
         <div>
           <label htmlFor="password">PASSWORD</label>
-          <input id="password" type="password"/>
+          <input onChange={handleChange} name="password" id="password" type="password"/>
         </div>
         
         <div>
