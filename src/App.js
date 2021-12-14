bc335ef9e4af513e80a8930d2aeea8c183169d5c
Benjamin Craddock <benjamincraddock@benjamins-mbp.lan>
2021-12-14 15:41:36 -0500

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 
+const Login = () => {
+  return (
+    <h2>LOGIN.</h2>
+  )
+}
+
+const FriendList = () => {
+  return (
+    <h2>FRIENDLIST.</h2>
+  )
+}
+
+const AddFriend = () => {
+  return (
+    <h2>ADDFRIEND.</h2>
+  )
+}
+ 
 function App() {
   return (
-    <div className="App">
-      <h2>Client Auth Project</h2>
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/">
+          <Login/>
+        </Route>
+
+        <Route exact path="/login">
+          <Redirect to="/"/>
+        </Route>
+
+        <Route exact path="/friend">
+          <FriendList/>
+        </Route>
+
+        <Route exact path="/friend/add">
+          <AddFriend/>
+        </Route>
+      </div>
+    </Router>
   );
 }
 
